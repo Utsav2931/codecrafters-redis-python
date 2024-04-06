30a0d58491ef4a6054a873f1c2df59693656d9e1
Utsav <upatel231a@gmail.com>
2024-04-05 21:39:05 -0700

    Added threading to upport for multiple concurrent clients.

--- app/main.py
+++ app/main.py
@@ -1,6 +1,13 @@
 # Uncomment this to pass the first stage
 import socket
+import threading
 
+def on_new_client(conn):
+    while True:
+        data = conn.recv(1024)
+        print("Received data", data)
+        pong = "+PONG\r\n"
+        conn.send(pong.encode())
 
 def main():
     # You can use print statements as follows for debugging, they'll be visible when running tests.
@@ -9,11 +16,10 @@ def main():
     # Uncomment this to pass the first stage
 
     server_socket = socket.create_server(("localhost", 6379), reuse_port=True)
-    conn, addr = server_socket.accept() # wait for client
-    while True: 
-        data = conn.recv(1024)
-        conn.send("+PONG\r\n".encode())
-
+    while True:
+        conn, addr = server_socket.accept()
+        t = threading.Thread(target = on_new_client, args=(conn,)) # Start a new thread procee when a client connects.
+        t.start()
 
 if __name__ == "__main__":
     main()
